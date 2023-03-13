ea2a06c0871c0cbb4908b191062a35b472304458
IonaHiggins <ihiggi200@caledonian.ac.uk>
2023-03-13 18:02:01 +0000

    ADDED THE MAP FINALLY

--- Floc/index_maps.js
+++ Floc/index_maps.js
@@ -1,102 +1,17 @@
-Skip to content
-Search or jump to…
-Pulls
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@areej206 
-areej206
-/
-IP3Project
-Public
- Unwatch 1
-0Cannot fork because you own this repository and are not a member of any organizations.
- Star 0
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-More
-main
-IP3Project/Google API/Google Maps/index.js /Jump to
-@IonaHiggins
-IonaHiggins Added Google API
-Latest commit 163e4dc 12 minutes ago
- History
- 1 contributor
-51 lines (45 sloc)  1.52 KB
-
-let map, infoWindow;
-
+// Initialize and add the map
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
   });
-  infoWindow = new google.maps.InfoWindow();
-
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
   });
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
 }
 
 window.initMap = initMap;
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-IP3Project/index.js at main · areej206/IP3Project
-Total Adblock Logo
-Remove Ads
-Total Adblock Logo
