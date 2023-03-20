4f94392f7e6412944f8584a601bf198cab1c0f2a
areej206 <99836322+areej206@users.noreply.github.com>
2023-03-20 14:32:49 +0000

    Flocccccc

--- Floc/googleMaps/index_maps.js
+++ Floc/googleMaps/index_maps.js
@@ -1,13 +1,27 @@
+// var cssId = 'myCss';  // you could encode the css path itself to generate id..
+// if (!document.getElementById(cssId))
+// {
+//     var head  = document.getElementsByTagName('head')[0];
+//     var link  = document.createElement('link');
+//     link.id   = cssId;
+//     link.rel  = 'stylesheet';
+//     link.type = 'text/css';
+//     link.href = 'http://website.example/css/stylesheet.css';
+//     link.media = 'all';
+//     head.appendChild(link);
+// }
+
 let map, infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 55.8642, lng: 0.0133 },
     zoom: 6,
   });
   infoWindow = new google.maps.InfoWindow();
 
   const locationButton = document.createElement("button");
+  locationButton.style.fontSize = "20px";
 
   locationButton.textContent = "Pan to Current Location";
   locationButton.classList.add("custom-map-control-button");
@@ -21,6 +35,7 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
+           //.style.fontSize = "20px";
 
           infoWindow.setPosition(pos);
           infoWindow.setContent("Location found.");
@@ -49,3 +64,5 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 }
 
 window.initMap = initMap;
+
+// Style
